58662faa1fd56087587efcbab2a1c83fcb56771a
milkho <michael.khoroshun@gmail.com>
2017-12-27 19:48:35 +0200

    Rewrited with arrow functions

--- test/BallotTest.js
+++ test/BallotTest.js
@@ -1,99 +1,99 @@
 var Ballot = artifacts.require("Ballot");
 
-contract('Ballot', function(accounts) {
+contract('Ballot', accounts => {
 
   //testing contract's constructor
-  it("should initialize the contract owner as the chairperson", function() {
-    var ballot;
-    return Ballot.deployed().then(function(instance) {
+  it("should initialize the contract owner as the chairperson", () => {
+    let ballot;
+    return Ballot.deployed().then(instance => {
       ballot = instance;
       return ballot.chairperson.call();
-    }).then(function(chairperson) {
+    }).then(chairperson => {
       assert.equal(chairperson, accounts[0]);
-    })
+    });
   });
 
 
-  it("should be initialized with two proposals", function() {
-    var ballot;
-    return Ballot.deployed().then(function(instance) {
+  it("should be initialized with two proposals", () => {
+    let ballot;
+    return Ballot.deployed().then(instance => {
       ballot = instance;
       return ballot.proposals.call(0);
-    }).then(function(firstProposal) {
+    }).then(firstProposal => {
       let first = web3.toUtf8(firstProposal[0]);
       assert.equal(first, "first proposal", "first proposal should have been created");
       return ballot.proposals.call(1);
-    }).then(function(secondProposal) {
+    }).then(secondProposal => {
       let second = web3.toUtf8(secondProposal[0]);
       assert.equal(second, "second proposal", "second proposal should have been created");
-    })
+    });
   });
 
 
   //modifying the state of the contract
-  it("should allow a registered voter to vote on a proposal", function() {
-    var ballot;
-    var allowedVoter = accounts[1];
-    var notAllowedVoter = accounts[2];
-    return Ballot.deployed().then(function(instance) {
+  it("should allow a registered voter to vote on a proposal", () => {
+    let ballot;
+    let allowedVoter = accounts[1];
+    let notAllowedVoter = accounts[2];
+    return Ballot.deployed().then(instance => {
       ballot = instance;
-      return ballot.giveRightToVote(allowedVoter)
-    }).then(function() {
+      return ballot.giveRightToVote(allowedVoter);
+    }).then(() => {
       return ballot.voters.call(allowedVoter);
-    }).then(function(voter) {
+    }).then(voter => {
       let weight = voter[0].toNumber();
-      assert.equal(weight, 1, 'allowed voter do not have rights to vote');
-    })
-  })
+      assert.equal(weight, 1, 'allowed voter dont have rights to vote');
+    });
+  });
 
 
-  it("should not allow an unregistered voter to vote on a proposal", function() {
-    var ballot;
-    var notAllowedVoter = accounts[2];
-    return Ballot.deployed().then(function(instance) {
+  it("should not allow an unregistered voter to vote on a proposal", () => {
+    let ballot;
+    let notAllowedVoter = accounts[2];
+    return Ballot.deployed().then(instance => {
       ballot = instance;
       return ballot.voters.call(notAllowedVoter);
-    }).then(function(voter) {
+    }).then(voter => {
       let weight = voter[0].toNumber();
       assert.equal(weight, 0, 'not allowed voter has rights to vote');
-    })
-  })
+    });
+  });
 
 
-  it("should be able to delegate", function() {
-    var ballot;
-    var delegator = accounts[0];
-    var delegatee = accounts[3];
-    return Ballot.deployed().then(function(instance) {
+  it("should be able to delegate", () => {
+    let ballot;
+    let delegator = accounts[0];
+    let delegatee = accounts[3];
+    return Ballot.deployed().then(instance => {
       ballot = instance;
       return ballot.giveRightToVote(delegatee)
-    }).then(function() {
+    }).then(() => {
       return ballot.delegate(delegatee  )
-    }).then(function() {
+    }).then(() => {
       return ballot.voters.call(delegatee);
-    }).then(function(delegatee) {
+    }).then(delegatee => {
       let weight = delegatee[0].toNumber();
       assert.equal(weight, 2, "delegatee shoud have weight equals 2");
-    })
+    });
   });
 
 
   it("should be able to vote", function() {
-    var ballot;
-    var votesCountBefore;
-    var votesCountAfter;
-    return Ballot.deployed().then(function(instance) {
+    let ballot;
+    let votesCountBefore;
+    let votesCountAfter;
+    return Ballot.deployed().then(instance => {
       ballot = instance;
       return ballot.proposals.call(0);
-    }).then(function(firstProposal) {
+    }).then(firstProposal => {
       votesCountBefore = firstProposal[1].toNumber();
       return ballot.vote(0, {from: accounts[1]});
-    }).then( function()  {
+    }).then( () =>  {
       return ballot.proposals.call(0);
-    }).then(function(firstProposal)  {
+    }).then(firstProposal =>  {
       votesCountAfter = firstProposal[1].toNumber();
       return ballot.voters.call(accounts[1]);
-    }).then(function(voter) {
+    }).then(voter => {
       let weight = voter[0];
       let isVoted = voter[1];
       let votedProposal = voter[3];
@@ -106,20 +106,20 @@ contract('Ballot', function(accounts) {
   });
 
 
- it("should be able to choose winner", function() {
-    var ballot;
-    var winnerIndex = 0;
-    var winnerName = "first proposal";
-    return Ballot.deployed().then(function(instance) {
+ it("should be able to choose winner", () => {
+    let ballot;
+    let winnerIndex = 0;
+    let winnerName = "first proposal";
+    return Ballot.deployed().then(instance => {
       ballot = instance;
-      return ballot.vote(winnerIndex, {from: accounts[1]});
-    }).then(function() {
+      return ballot.vote(winnerIndex, {from: accounts[3]});
+    }).then(() => {
       return ballot.winningProposal.call();
-    }).then(function(winningProposal) {
+    }).then(winningProposal => {
       assert.equal(winningProposal, winnerIndex, "Winner index must be equal to winning proposal value");
       return ballot.winnerName.call();
-    }).then(function(proposalName) {
-       winnerProposalName = web3.toUtf8(proposalName);
+    }).then(proposalName => {
+      winnerProposalName = web3.toUtf8(proposalName);
       assert.equal(winnerProposalName, winnerName, "Winning proposal name must be equal to first proposal name");
     });
   });
